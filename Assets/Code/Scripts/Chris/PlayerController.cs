26dffea3bf721a57d7fc211aebe17d680c28e63b
Christopher Gix <chrisgix97@gmail.com>
2022-07-15 13:22:55 -0700

    Folder Structure and new Scene
    Added more folder structure and made a scene with some basic board game like movement

---
+++ Assets/Code/Scripts/Chris/PlayerController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public int spaceOn;
+
+    public UnityEngine.GameObject Board;
+    Transform[] boardSpaces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //To keep things organized, remove parent
+        movePoint.parent = null;
+
+        //Get transforms of board spaces
+        boardSpaces = Board.GetComponentsInChildren<Transform>();
+
+        spaceOn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+
+
+            if (Input.GetAxisRaw("Horizontal") == 1f && !(spaceOn + 1 > boardSpaces.Length - 1))
+            {
+                spaceOn++;
+                movePoint.position = (boardSpaces[spaceOn].position);
+            }
+            if (Input.GetAxisRaw("Horizontal") == -1f && !(spaceOn - 1 < 0))
+            {
+                spaceOn--;
+                movePoint.position = (boardSpaces[spaceOn].position);
+            }
+
+            if (Input.GetAxisRaw("Vertical") == 1f && !(spaceOn + 2 > boardSpaces.Length - 1))
+            {
+                spaceOn += 2;
+                movePoint.position = (boardSpaces[spaceOn].position);
+            }
+            if (Input.GetAxisRaw("Vertical") == -1f && !(spaceOn - 2 < 0))
+            {
+                spaceOn -= 2;
+                movePoint.position = (boardSpaces[spaceOn].position);
+            }
+        }
+    }
+}
